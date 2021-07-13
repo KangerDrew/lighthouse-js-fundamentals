117342ca7ec9ea27cbb0dbddc44902d27a73b49d
Drew Kang <kangdrew95@gmail.com>
2021-07-13 03:07:07 +0000

    After changing url via Solene's method

---
+++ hello.js
@@ -0,0 +1,10 @@
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+
+console.log("This is to test if GitHub is working or not.")
